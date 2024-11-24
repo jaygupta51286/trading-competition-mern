6e20ca39e2806f8ea83f4bb2ac9b4004a00a7a4c
Jay <jaygupta51286@gmail.com>
2024-11-24 20:11:02 +0530

    Update app js for better visibility

--- client/src/App.js
+++ client/src/App.js
@@ -75,10 +75,10 @@ The only event where participants are allowed to withdraw their profits with no 
 The Only Event which is not bounded with any rules i.e. you can take any support from the community / Tools / Strategies / Team.
 `} />}
             />
-            <Route path="/winning-criteria" element={<InfoSection sectionId="winning-criteria" title="Winning Criteria" content={`
+            <Route path="/conditions" element={<InfoSection sectionId="conditions" title="Winning Criteria" content={`
 Make Highest Profit
 Make maximum withdrawals of the Profit (Count Wise and Amount Wise)
-All the participants who have make the profits with the round off the decimal values. (i.e. 100.01 to 100.50 will be considered 100 and 100.51-100.99 will be considered 101)
+All the participants who have made the profits with the round off the decimal values. (i.e. 100.01 to 100.50 will be considered 100 and 100.51-100.99 will be considered 101)
 All participants who are in the tie then maximum withdrawals will be considered to finalize the winner participant.
 `} />}
             />
