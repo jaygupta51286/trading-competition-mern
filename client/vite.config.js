881b607958f473dd36c27fc718cc6a75a63f9938
Jay <jaygupta51286@gmail.com>
2024-12-15 01:35:49 +0530

    index.html for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
+  base: process.env.NODE_ENV === 'production' ? './' : '/', // Adjust base path
 });
--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ app.use('/api/leaderboard', leaderboardRoutes);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../dist/build')));
 
   // Serve index.html for all non-API routes
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../dist/build', 'index.html'));
   });
 }
 
